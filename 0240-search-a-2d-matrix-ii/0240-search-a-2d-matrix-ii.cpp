3babb44a5fab99c0169d4375b0c0a663e15fc9ea
Aryan Prajapati <prajapatiaryan113@gmail.com>
2026-07-14 10:20:49 +0530

    Time: 63 ms (20.13%), Space: 18.8 MB (38.81%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,22 +3,15 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
       int n = matrix.size();
         int m = matrix[0].size();
-
-         for (int i = 0; i < n; i++) {
-            if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
-                int low = 0, high = m - 1;
-
-                while (low <= high) {
-                    int mid = (low + high) / 2;
-
-                    if (matrix[i][mid] == target)
-                        return true;
-                    else if (matrix[i][mid] < target)
-                        low = mid + 1;
-                    else
-                        high = mid - 1;
-                }
-            }
+        int i=0;
+        int j=m-1;
+        while(i<n&&j>=0){
+            if(matrix[i][j]==target)
+            return true;
+            else if(matrix[i][j]>target)
+            j--;
+            else
+            i++;
         }
         return false;
     }
