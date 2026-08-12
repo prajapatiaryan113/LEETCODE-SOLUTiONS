89e49ccd3d820b4846f656443fca38a3556d7c57
Aryan Prajapati <prajapatiaryan113@gmail.com>
2026-08-12 11:03:59 +0530

    Time: 0 ms (100%), Space: 12.6 MB (73.83%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int start = 0, end = arr.size() - 1;
+         while (start < end) {
+        int mid = start + (end - start) / 2;
+        
+        // Minimum is in the right half
+        if (arr[mid] > arr[mid+1]) { 
+            end = mid ;
+        } 
+        // Minimum is in the left half (including mid)
+        else { 
+            start=mid+1;
+        }
+    }
+    return start;
+    }
+};
